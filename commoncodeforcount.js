9ddbefa17ca685df9985b3fc2667da7bd98ac8cd
muniram72071 <munirathnam7721@gmail.com>
2025-05-14 22:24:08 +0530

    add some changes for large screen mobile responsive

--- commoncodeforcount.js
+++ commoncodeforcount.js
@@ -2,4 +2,4 @@ document.addEventListener('DOMContentLoaded', () => {
                 let cart = JSON.parse(localStorage.getItem('cart')) || [];
                 const cartCount = cart.reduce((acc, item) => acc + item.quantity, 0);
                 document.getElementById('cart-count').textContent = cartCount;
-            });
+        });
